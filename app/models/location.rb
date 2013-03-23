ab393514fec24eaf28127b2eac39643a5f9e9be8
FotoVerite <mzbphoto@gmail.com>
2013-03-22 23:16:22 -0400

    checkboxes for registrion form

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,12 @@
 class Location < ActiveRecord::Base
 
   has_many :members
+
+  TIMES = {
+    1 => "9-11",
+    2 => "11-1",
+    3 => "1-3",
+    4 => "3-5"
+  }
+
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,6 @@
 class Member < ActiveRecord::Base
   belongs_to :location
+
+  serialize :times
+
 end
