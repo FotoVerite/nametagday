d3dfea5cd4028f8589af8f5d0af33f1e4fea6b47
Julia K <julia326@gmail.com>
2013-04-03 03:43:22 -0400

    Added more maps locations. Wired up times picker (now comma-separated string of times keys). Timeslots now toggle for leader and normal shifts. Map icons respond to clicking. Added small form validator.

--- app/assets/javascripts/registration.js
+++ app/assets/javascripts/registration.js
@@ -1,84 +1,111 @@
-function makeMarker(title, position, location_id, num_needed, map) {
-	var blueCircle = {
+var markers = [];
+
+// Returns an icon based on volunteer availability. (Right now just takes ratio of total volunteers
+// to total needed volunteers across all time slots. Can change to arbitrary whatever.)
+function makeIcon(needMap, haveMap) {
+	var sumHave = 0;
+	var sumNeed = 0;
+	for (i in needMap) {
+		sumNeed += needMap[i];
+	}
+	for (i in haveMap) {
+		sumHave += haveMap[i];
+	}
+	
+	// fill color depends on the combination of what we have and need at various times
+	var ratio = sumHave / sumNeed;
+	var fillColor;
+	if (ratio == 1) fillColor = "black";
+	else if (ratio < 0.3) fillColor = "gray";
+	else fillColor = "blue";
+	var icon = {
 		path: google.maps.SymbolPath.CIRCLE,
-		fillColor: num_needed > 5 ? "green" : "red",
+		fillColor: fillColor,
 		fillOpacity: 0.8,
 		scale: 5,
 		strokeWeight: 0.5
 	}
+	return icon;
+}
+
+function makeMarker(title, position, location_id, needMap, haveMap) {
+	var icon = makeIcon(needMap, haveMap);
 	var marker = new google.maps.Marker({
 		position: position,
 		title: title,
-		icon: blueCircle,
-		map: map
+		icon: makeIcon(needMap, haveMap)
 	});
+	
+	// writes arbitrary values into dict
 	marker.setValues({location_id: location_id});
 	
 	google.maps.event.addListener(marker, 'click', function() {
-		// todo: enlarge icon if selected
-		map.setCenter(marker.getPosition());
-		document.forms[0].elements["location_id"].value = marker.get("location_id");
+		$('#location_id').val(marker.get("location_id"));
+		
+		// make the selected marker large, all others smaller
+		for (i in markers) {
+			icon = markers[i].getIcon();
+			icon.scale = markers[i].get("location_id") == marker.get("location_id") ? 10 : 5;
+			markers[i].setIcon(icon);
+		}
+		
+		// todo: update displayed button colors
+		
 	});
 	
 	return marker;
 }
 
-// Returns an array of LocationIdMarkers.
-function loadMarkers(map) {
-
-	// Manhattan
-	/*addMarker(map, "Washington Square", new google.maps.LatLng(40.730869, -73.997726));
-	addMarker(map, "Bryant Park", new google.maps.LatLng(40.753564, -73.982878));
-	addMarker(map, "Union Square", new google.maps.LatLng(40.735551, -73.990345));
-	addMarker(map, "Tompkins Square Park", new google.maps.LatLng(40.726576, -73.981848));
-	addMarker(map, "Morningside Heights", new google.maps.LatLng(40.806468, -73.958502));
-	addMarker(map, "Fort Tryon Park", new google.maps.LatLng(40.862836, -73.931465));
-	addMarker(map, "Upper West Side", new google.maps.LatLng(40.785676, -73.970604));
-	addMarker(map, "Great Lawn", new google.maps.LatLng(40.781256, -73.966527));
-	addMarker(map, "Columbus Circle Area", new google.maps.LatLng(40.768029, -73.981977));
-	addMarker(map, "Midtown East", new google.maps.LatLng(40.751386, -73.976011));
-	addMarker(map, "Lower Manhattan", new google.maps.LatLng(40.712947, -74.005923));*/
+// Populates markers array.
+function loadMarkers() {
+	$.getJSON('locations.json', function(data) {
+	  var places = data['locations']
+	  var times = data['times'];
+	  for (i in places) {
+	    var position = new google.maps.LatLng(places[i]['lat'], places[i]['lng']);
+	  	var markerTitle = places[i]['name'];
+     	var location_id = places[i]['id'];
+		var need = {};
+		var have = places[i]['time_counts'];
+		for (var time in times) {
+		  need[time] = places[i]['target_distribution_sites'] * 6;
+		}
 	
-	// Getting from some other JS: lat, long, display name, location ID, # people needed.
-	// Test data for now.
-	var marker_descs = [];
-	
-	var marker1 = []
-	marker1.push("40.73");
-	marker1.push("-73.99");
-	marker1.push("Washington Square");
-	marker1.push("101");
-	marker1.push("3");
-	marker_descs.push(marker1);
-	
-	var marker2 = []
-	marker2.push("40.75");
-	marker2.push("-73.98");
-	marker2.push("Bryant Park");
-	marker2.push("102");
-	marker2.push("10");
-	marker_descs.push(marker2);
-	
-	var markers = []
-	for (i in marker_descs) {
-		var position = new google.maps.LatLng(eval(marker_descs[i][0]), eval(marker_descs[i][1]));
-		var title = marker_descs[i][2];
-		var location_id = marker_descs[i][3];
-		var num_needed = eval(marker_descs[i][4]);
-		markers.push(makeMarker(title, position, location_id, num_needed, map));
-	}
-	
-	return markers;
+		var marker = makeMarker(markerTitle, position, location_id, need, have);
+		markers.push(marker);
+	  }
+    });
 }
 
-// Runs on window loading.
+// Runs on window loading. Markers already created, so just assigns the map to them.
 function initializeMap() {
 	var mapOptions = {
 		center: new google.maps.LatLng(40.735551, -73.990345),
-		zoom: 11,
+		zoom: 12,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
 	
 	var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-	var markers = loadMarkers(map);
+	for (i in markers) {
+		markers[i].setMap(map);
+	}
+}
+
+function validateForm() {
+  // check if user entered location
+  if ($('#location_id').val() == '') {
+    alert('Please specify a location');
+    return false;
+  }
+  
+  // iterate through buttons to see which are active
+  var times = '';
+  $('.btn-group button').each(function() {
+    if ($(this).hasClass('active')) {
+      times += $(this).val() + ',';
+    }
+  });
+  $('#times').val(times);
+  
+  return true;
 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -30,17 +30,74 @@ locations << FactoryGirl.create(:location,
 								:name => "Union Square",
 								:lat => 40.7361,
 								:lng => -73.9901,
-								:time_counts => empty_times)
+								:time_counts => empty_times,
+								:target_distribution_sites => 10)
 locations << FactoryGirl.create(:location,
 								:name => "Washington Square Park",
 								:lat => 40.7308,
 								:lng => -73.9975,
-								:time_counts => empty_times)
+								:time_counts => empty_times,
+								:target_distribution_sites => 9)
 locations << FactoryGirl.create(:location,
 								:name => "Times Square",
 								:lat => 40.7566,
 								:lng => -73.9863,
-								:time_counts => empty_times)
+								:time_counts => empty_times,
+								:target_distribution_sites => 8)
+locations << FactoryGirl.create(:location,
+								:name => "Bryant Park",
+								:lat => 40.753564,
+								:lng => -73.982878,
+								:time_counts => empty_times,
+								:target_distribution_sites => 8)
+locations << FactoryGirl.create(:location,
+								:name => "Tompkins Square Park",
+								:lat => 40.726576,
+								:lng => -73.981848,
+								:time_counts => empty_times,
+								:target_distribution_sites => 8)
+locations << FactoryGirl.create(:location,
+								:name => "Morningside Heights",
+								:lat => 40.806468,
+								:lng => -73.958502,
+								:time_counts => empty_times,
+								:target_distribution_sites => 8)
+locations << FactoryGirl.create(:location,
+								:name => "Fort Tryon Park",
+								:lat => 40.862836,
+								:lng => -73.931465,
+								:time_counts => empty_times,
+								:target_distribution_sites => 8)
+locations << FactoryGirl.create(:location,
+								:name => "Upper West Side",
+								:lat => 40.785676,
+								:lng => -73.970604,
+								:time_counts => empty_times,
+								:target_distribution_sites => 8)
+locations << FactoryGirl.create(:location,
+								:name => "Great Lawn",
+								:lat => 40.781256,
+								:lng => -73.966527,
+								:time_counts => empty_times,
+								:target_distribution_sites => 8)
+locations << FactoryGirl.create(:location,
+								:name => "Columbus Circle Area",
+								:lat => 40.768029,
+								:lng => -73.981977,
+								:time_counts => empty_times,
+								:target_distribution_sites => 8)
+locations << FactoryGirl.create(:location,
+								:name => "Midtown East",
+								:lat => 40.751386,
+								:lng => -73.976011,
+								:time_counts => empty_times,
+								:target_distribution_sites => 8)
+locations << FactoryGirl.create(:location,
+								:name => "Lower Manhattan",
+								:lat => 40.712947,
+								:lng => -74.005923,
+								:time_counts => empty_times,
+								:target_distribution_sites => 8)
 
 ActiveRecord::Base.connection.execute("TRUNCATE members")
 1000.times do
