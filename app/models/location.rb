b9f5c7d435c170318cf4b20bfca6619da2e31aba
Tim O'Donnell <timodonnell@gmail.com>
2013-04-15 21:00:43 -0400

    working on editing

--- app/models/location.rb
+++ app/models/location.rb
@@ -14,5 +14,12 @@ class Location < ActiveRecord::Base
     4 => "9am - 1pm (as a coordinator)",
     5 => "1pm - 5pm (as a coordinator)",
   }
+  IS_COORDINATOR_TIME = {
+  	1 => false,
+  	2 => false,
+  	3 => false,
+  	4 => true,
+  	5 => true,
+  }
 
 end
