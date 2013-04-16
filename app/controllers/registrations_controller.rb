fab5a17dd501d95f6e3031b0e395131e13729cd9
FotoVerite <mzbphoto@gmail.com>
2013-04-16 01:18:19 -0400

    [cntrl] another typo

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -10,9 +10,9 @@ class RegistrationsController < ApplicationController
     @member.times = []
     @member.location_id = params[:location_id]
     @member.attributes = params[:member]
-    digest_friends(params[:friends])
-        @member.times = params[:times].split(',').collect{|val| val.strip.to_i}.select{|val| Location::TIMES.has_key? val }
+    @member.times = params[:times].split(',').collect{|val| val.strip.to_i}.select{|val| Location::TIMES.has_key? val }
 
+    digest_friends(params[:friends])
     @leader = Friend.find_by_registration_token(params[:registration_token]).member if params[:registration_token].present?
     @member.referer = @leader
     if @member.save
