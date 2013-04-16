588e68ac69888e13094951d87d71dcb0211c5560
FotoVerite <mzbphoto@gmail.com>
2013-04-15 23:09:19 -0400

    added mailjet

---
+++ config/initializers/mailjet.rb
@@ -0,0 +1,6 @@
+# initializers/mailjet.rb
+Mailjet.configure do |config|
+  config.api_key = '16ede54ac6c29d717b69fa643384b55e'
+  config.secret_key = 'your-secret-key'
+  config.default_from = 'my_registered_mailjet_email@domain.com'
+end
