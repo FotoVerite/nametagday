140da2a7e454efe9d3144ce6ab2f4e5a08d7905e
FotoVerite <mzbphoto@gmail.com>
2013-04-15 23:10:38 -0400

    typo fix

--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -2,5 +2,5 @@
 Mailjet.configure do |config|
   config.api_key = '16ede54ac6c29d717b69fa643384b55e'
   config.secret_key = 'your-secret-key'
-  config.default_from = 'my_registered_mailjet_email@domain.com'
+  config.default_from = 'hello@nametagday.com'
 end
