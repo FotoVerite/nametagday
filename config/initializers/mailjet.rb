516d1a63e43d38b8b12f47c44a97b26bc4240c56
FotoVerite <mzbphoto@gmail.com>
2013-04-16 00:13:06 -0400

    [mailjet] added secret key

--- config/initializers/mailjet.rb
+++ config/initializers/mailjet.rb
@@ -1,6 +1,6 @@
 # initializers/mailjet.rb
 Mailjet.configure do |config|
   config.api_key = '16ede54ac6c29d717b69fa643384b55e'
-  config.secret_key = 'your-secret-key'
+  config.secret_key = 'e151c8ce5738b35e5bc58392a28228d5'
   config.default_from = 'hello@nametagday.com'
 end
